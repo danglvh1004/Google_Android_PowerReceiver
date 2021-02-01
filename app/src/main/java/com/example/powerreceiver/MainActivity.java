c78f68caea751a5f74ac9724ad9a448c2448569b
danglvh1004 <lvhd.hdlv@gmail.com>
2021-02-01 10:16:08 +0700

    Task 2. Send and receive a custom broadcast

--- app/src/main/java/com/example/powerreceiver/MainActivity.java
+++ app/src/main/java/com/example/powerreceiver/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.powerreceiver;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     private CustomReceiver mReceiver = new CustomReceiver();
+
+    private static final String ACTION_CUSTOM_BROADCAST =
+            BuildConfig.APPLICATION_ID + ".ACTION_CUSTOM_BROADCAST";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +25,23 @@ public class MainActivity extends AppCompatActivity {
         filter.addAction(Intent.ACTION_POWER_CONNECTED);
 
         this.registerReceiver(mReceiver, filter);
+
+        LocalBroadcastManager.getInstance(this)
+                .registerReceiver(mReceiver,
+                        new IntentFilter(ACTION_CUSTOM_BROADCAST));
     }
 
     @Override
     protected void onDestroy() {
         this.unregisterReceiver(mReceiver);
+        LocalBroadcastManager.getInstance(this)
+                .unregisterReceiver(mReceiver);
         super.onDestroy();
+    }
+
+    public void sendCustomBroadcast(View view) {
+
+        Intent customBroadcastIntent = new Intent(ACTION_CUSTOM_BROADCAST);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(customBroadcastIntent);
     }
 }
